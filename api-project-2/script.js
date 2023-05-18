e314358f5227ff6d85f0aa2ee5d257f2de6712c9
Marius Ratnikas <marius.ratnikas@gmail.com>
2023-05-18 16:56:56 +0300

    task done

--- api-project-2/script.js
+++ api-project-2/script.js
@@ -1,23 +1,66 @@
-fetch('https://dog.ceo/api/breeds/list/all')
-    .then(res => res.json())
-    .then(dogData => {
-        console.log(dogData.message)
-        const dogBreedsList = dogData.message;
-        const dogSelectorElement = document.querySelector('#dog-list');
-        for (const dogBreed in dogBreedsList) {
-            const dogBreedElement = document.createElement('option');
-            const capDogBreed = dogBreed.charAt(0).toUpperCase() + dogBreed.slice(1)
-            if (dogBreedsList[dogBreed].length === 0) {
-                dogBreedElement.textContent = `${capDogBreed}`;
-                dogBreedElement.dataset.breed = dogBreed;
-            } else {
-                dogBreedsList[dogBreed].forEach(subBreed => {
-                    const capSubBreed = subBreed.charAt(0).toUpperCase() + dogBreed.slice(1)
-                    dogBreedElement.textContent = `${capDogBreed}: ${capSubBreed}`;
-                    dogBreedElement.dataset.breed = dogBreed;
-                    dogBreedElement.dataset.subBreed = subBreed;
-                })
+const pictureDivElelemnt = document.querySelector('#picture');
+const pictureElelemnt = document.createElement('img');
+
+(function dogBreedSelector() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+        .then(res => res.json())
+        .then(dogData => {
+            const dogBreedsList = dogData.message;
+            const dogSelectorElement = document.querySelector('#dog-list');
+            for (const dogBreed in dogBreedsList) {
+                const capDogBreed = dogBreed.charAt(0).toUpperCase() + dogBreed.slice(1)
+                if (dogBreedsList[dogBreed].length === 0) {
+                    const dogBreedElement = document.createElement('option');
+                    dogBreedElement.textContent = `${capDogBreed}`;
+                    dogBreedElement.value = dogBreed;
+                    dogSelectorElement.append(dogBreedElement)
+                } else {
+                    dogBreedsList[dogBreed].forEach(subBreed => {
+                        const dogBreedElement = document.createElement('option');
+                        const capSubBreed = subBreed.charAt(0).toUpperCase() + subBreed.slice(1)
+                        dogBreedElement.textContent = `${capDogBreed}: ${capSubBreed}`;
+                        dogBreedElement.value = `${dogBreed}-${subBreed}`;
+                        dogSelectorElement.append(dogBreedElement)
+                    })
+                }
             }
-            dogSelectorElement.append(dogBreedElement)
-        }
+        })
+})();
+
+(function dogSearchFormAction() {
+    const dogSearchFormElement = document.querySelector('#dog-search')
+    dogSearchFormElement.addEventListener('submit', event => {
+        event.preventDefault()
+        const selectedDogValue = event.target['dog-list'].value;
+        getDogPicture(selectedDogValue)
+
     })
+})();
+
+function getDogPicture(selectedDogValue) {
+    let fetchAddress;
+    if (selectedDogValue.includes('-')) {
+        const dogBreedAndSubBreed = selectedDogValue.split('-');
+        const breed = dogBreedAndSubBreed[0];
+        const subBreed = dogBreedAndSubBreed[1];
+        fetchAddress = `https://dog.ceo/api/breed/${breed}/${subBreed}/images/random`;
+    } else {
+        fetchAddress = `https://dog.ceo/api/breed/${selectedDogValue}/images/random`;
+    }
+
+    fetch(fetchAddress)
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error(res.status);
+            }
+        })
+        .then(dogData => {
+            console.log(dogData.message)
+            console.dir(pictureElelemnt)
+            pictureElelemnt.src = dogData.message;
+            pictureDivElelemnt.append(pictureElelemnt)
+        })
+        .catch(error => console.error(error))
+}
